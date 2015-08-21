561bd1175555321b1c839a90e181e9dd544e72f7
bloodywing <bloodywing@neocomy.net>
2015-08-21 18:40:20 +0200

    Kwick initial commit
    Working Login/Logout
    Index Page, show a Feed.

---
+++ kwick/kwick.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 21 17:50:30 2015
+
+@author: Tilra
+"""
+
+import hashlib
+import requests
+import random
+
+version = 1.0  # Kwicks mapi version
+
+class KwickError(Exception):
+
+    def __init__(self, msg):
+        self.msg = msg['errorMsg']
+    
+    def __str__(self):
+        return repr(self.msg)
+
+class Kwick(object):
+
+    session = None
+    cookie = None
+    
+    host = 'http://mapi.kwick.de/{version}'.format(version=version)
+    response = None
+    
+    def __init__(self, session=None):
+        if not session:
+            self.session = requests.Session()
+    
+    def post(self, url, data, json=True):
+        response = self.session.post(self.host + url, data)
+        self.response = response
+        if json:
+            return response.json()
+        return response.content
+    
+    def get(self, url, params=dict(), json=True):
+        response = self.session.get(self.host + url, params=params)
+        self.response = response
+        if json:
+            return response.json()
+        return response.content
+    
+    def kwick_login(self, kwick_username, kwick_password):
+        """
+        Wenn erfolgreich:
+            {'loggedIn': True,
+             'session_id': 'ein hash',
+             'session_name': 'a2K3j8G1',
+             'userid': XXXXXXX}
+        Der name der Session ist immer gleich
+        """
+        url = '/login'
+        data = dict(
+            kwick_username=kwick_username,
+            kwick_password=kwick_password
+        )
+        json = self.post(url, data)
+        if 'errorMsg' in json:
+            raise KwickError(json)
+        else:
+            return json
+    
+    def kwick_logout(self):
+        """
+        Von kwick ausloggen
+        """
+        url = '/logout'
+        return self.get(url)
+    
+    def kwick_index(self, page):
+        """
+        docs: http://developer.kwick.com/index.php/User/index
+        """
+        url = '/index'
+        params = dict(
+            page=page
+        )
+        return self.get(url, params=params)
+    
+    def kwick_feed(self, feedid, delete=False):
+        """
+        docs: http://developer.kwick.com/index.php/Feed/feed
+        docs: http://developer.kwick.com/index.php/Feed/feed/feedid/delete
+        warning: veraltet
+        """
+        if delete:
+            url = '/feed/{feedid}/delete'.format(feedid=feedid)
+        else:
+            url = '/feed/{feedid}'.format(feedid=feedid)
+        return self.get(url)
