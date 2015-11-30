5727d52752171209941f205ceb88b6632f8aeec3
bloodywing <bloodywing@neocomy.net>
2015-11-30 20:54:46 +0100

    pypi integration

--- setup.py
+++ setup.py
@@ -22,14 +22,23 @@ requires = [
     'requests'
 ]
 
+here = os.path.dirname(os.path.abspath(__file__))
+f = open(os.path.join(here, 'README.md'))
+long_description = f.read().strip()
+f.close()
+
 setup(
     name='kwickmapi-python',
     version=kwick.__version__,
     description='A python library for kwick.de based on their mapi',
+    long_description=long_description,
+    url='https://github.com/bloodywing/kwickmapi-python',
     packages=packages,
     install_requires=requires,
+    requires=requires,
     test_suite = 'nose.collector',
     author=kwick.__author__,
     author_email='bloodywing@tastyespresso.de',
     license='GPL 3.0',
+    keywords=['kwick', 'api', 'socialmedia', 'website', 'library']
 )
